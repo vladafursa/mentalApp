62947c84fd0b47fc7665fdba460981e3bb1d0ab6
T0337485 <t0337485@my.ntu.ac.uk>
2025-03-20 00:48:43 +0000

    added register functionality

---
+++ mentalAppCoursework/ViewModels/RegisterViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewModel.swift
+//  mentalAppCoursework
+//
+//  Created by Влада Фурса on 20.03.25.
+//
+
+import SwiftUI
+
+struct RegisterViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterViewModel()
+}
